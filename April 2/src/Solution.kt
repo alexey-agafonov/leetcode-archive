b461b16a807f0e4fd6c8fc5433421fc4f07f355a
alexey-agafonov <aleks.xploid@gmail.com>
2020-04-03 03:00:47 +0300

    Add a solution for the April 2nd challendge.

---
+++ April 2/src/Solution.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun getSum(n: Int): Int {
+        var number = n
+        var pow = 0
+
+        while(number > 0){
+            val digit = number % 10
+            pow += digit * digit
+            number /= 10
+        }
+        return pow
+    }
+
+    fun isHappy(n: Int): Boolean {
+        val seen = hashSetOf<Int>()
+        var current = n
+
+        while (true) {
+            if (seen.contains(current)) return false
+            else if (current == 1) return true
+            seen.add(current)
+            current = getSum(current)
+        }
+    }
+}
